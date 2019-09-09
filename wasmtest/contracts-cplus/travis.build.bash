3624ccc206c8b0367236dcd12a748facd979ac13
steven <chenglin@ont.com>
2019-09-09 19:33:49 +0800

    update 444

--- wasmtest/contracts-cplus/travis.build.bash
+++ wasmtest/contracts-cplus/travis.build.bash
@@ -5,17 +5,17 @@ oldir=$(pwd)
 currentdir=$(dirname $0)
 cd $currentdir
 
-#git clone https://github.com/ontio/ontology-wasm-cdt-cpp
-#compilerdir="/home/cl/github/ontology-wasm-cdt-cpp/install/bin"
-wget https://github.com/ontio/ontology-wasm-cdt-cpp/blob/master/docker/Dockerfile?raw=true -O Dockerfile
-docker build -t ontowasm .
-docker run -v $(pwd):/root/contracts ontowasm '/bin/bash<<<"export PATH=$PATH:/ontowasm/bin; mkdir -p /root/contracts; cd /root/contracts; for f in *.cpp; do ont_cpp $f -lbase58 -lcrypto -lbuiltins -o  ${f%.cpp}.wasm; done"'
+git clone https://github.com/ontio/ontology-wasm-cdt-cpp
+compilerdir="./ontology-wasm-cdt-cpp/install/bin"
+#wget https://github.com/ontio/ontology-wasm-cdt-cpp/blob/master/docker/Dockerfile?raw=true -O Dockerfile
+#docker build -t ontowasm .
+#docker run -v $(pwd):/root/contracts ontowasm '/bin/bash<<<"export PATH=$PATH:/ontowasm/bin; mkdir -p /root/contracts; cd /root/contracts; for f in *.cpp; do ont_cpp $f -lbase58 -lcrypto -lbuiltins -o  ${f%.cpp}.wasm; done"'
 #compilerdir="./ontology-wasm-cdt-cpp/install/bin"
 
-#for f in *.cpp
-#do
-#	$compilerdir/ont_cpp $f -lbase58 -lcrypto -lbuiltins -o  ${f%.cpp}.wasm
-#done
+for f in *.cpp
+do
+	$compilerdir/ont_cpp $f -lbase58 -lcrypto -lbuiltins -o  ${f%.cpp}.wasm
+done
 
 mv *.wasm ../testwasmdata/
 rm *.wasm.str
