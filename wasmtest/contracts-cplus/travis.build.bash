642d8ef769f3ab7efaa58d126e837f3f1dcb5e5b
steven <chenglin@ont.com>
2019-09-09 17:57:42 +0800

    update 111

--- wasmtest/contracts-cplus/travis.build.bash
+++ wasmtest/contracts-cplus/travis.build.bash
@@ -7,7 +7,7 @@ cd $currentdir
 
 #git clone https://github.com/ontio/ontology-wasm-cdt-cpp
 #compilerdir="/home/cl/github/ontology-wasm-cdt-cpp/install/bin"
-wget https://github.com/ontio/ontology-wasm-cdt-cpp/blob/master/docker/Dockerfile
+wget https://github.com/ontio/ontology-wasm-cdt-cpp/blob/master/docker/Dockerfile?raw=true -O Dockerfile
 docker build -t ontowasm .
 docker run -v $(pwd):/root/contracts ontowasm '/bin/bash<<<"cd /root/contracts; for f in *.cpp; do ont_cpp $f -lbase58 -lcrypto -lbuiltins -o  ${f%.cpp}.wasm; done"'
 #compilerdir="./ontology-wasm-cdt-cpp/install/bin"
