f51d6571a193ceb01556e28f1bdbd0f122fd13ec
steven <chenglin@ont.com>
2019-09-26 11:31:07 +0800

    update

--- wasmtest/run-wasm-tests.sh
+++ wasmtest/run-wasm-tests.sh
@@ -9,16 +9,15 @@ set -x
 #fi
 
 if ! which clang-9 ; then
-	sudo apt remove clang-7 llvm llvm-7
 	wget releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 	tar xvf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-	export PATH=$PATH:"$(pwd)/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin"
+	export PATH="$(pwd)/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin":$PATH
 	cd clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin
-	for f in $(ls)
-	do
-		[[ -L $f ]] && sudo cp $f /usr/bin/
-	done
-	cd -
+	#for f in $(ls)
+	#do
+	#	[[ -L $f ]] && sudo cp $f /usr/bin/
+	#done
+	#cd -
 fi
 
 #rustup target add wasm32-unknown-unknown
