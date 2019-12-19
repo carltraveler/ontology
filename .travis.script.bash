5d650f58f81419b7fb9914ad4f60744d7e964ab9
steven <chenglin@ont.com>
2019-12-19 17:07:02 +0800

    fixed

--- .travis.script.bash
+++ .travis.script.bash
@@ -2,16 +2,19 @@
 
 VERSION=$(git describe --always --tags --long)
 
-if [ $TRAVIS_OS_NAME != 'windows' ]; then
-	echo "linux mod"
+if [ $TRAVIS_OS_NAME == 'linux' ]; then
+	echo "linux sys"
 	env GO111MODULE=on make all
 	env GO111MODULE=on go mod vendor
 	cd ./wasmtest && bash ./run-wasm-tests.sh && cd ../
 	bash ./.travis.check-license.sh
 	bash ./.travis.gofmt.sh
 	bash ./.travis.gotest.sh
+elif [ $TRAVIS_OS_NAME == 'osx' ]; then
+	echo "osx sys"
+	env GO111MODULE=on make all
 else
-	echo "windows mod"
+	echo "win sys"
 	env GO111MODULE=on CGO_ENABLED=1 go build  -ldflags "-X github.com/ontio/ontology/common/config.Version=${VERSION}" -o ontology-windows-amd64 main.go
 	env GO111MODULE=on go build  -ldflags "-X github.com/ontio/ontology/common/config.Version=${VERSION}" -o sigsvr-windows-amd64 sigsvr.go
 fi
