62a3ad986d3ed91434a4b280d61240caf1ac8bfa
steven <chenglin@ont.com>
2019-12-20 14:27:51 +0800

    fixed

--- smartcontract/service/wasmvm/wasmjit_runtime.go
+++ smartcontract/service/wasmvm/wasmjit_runtime.go
@@ -22,7 +22,7 @@ package wasmvm
 #cgo CFLAGS: -I.
 #cgo darwin LDFLAGS: -L. -lwasmjit_onto_interface_darwin -ldl -lc -lm
 #cgo linux LDFLAGS: -L. -lwasmjit_onto_interface -ldl -lc -lm
-#cgo windows LDFLAGS: -Wl,-rpath,${SRCDIR} -L. -lwasmjit_onto_interface -llibdl -llibc -llibm
+#cgo windows LDFLAGS: -Wl,-rpath,${SRCDIR} -L. -lwasmjit_onto_interface
 #include "wasmjit_runtime.h"
 #include <stdlib.h>
 */
